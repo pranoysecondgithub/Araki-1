ab78f5ff823a688a265d0ac8b7e1b1e50845bfcc
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-03-08 13:06:06 +0000

    unban fix

--- extras/unban.py
+++ extras/unban.py
@@ -13,6 +13,8 @@ class Unban(commands.Cog):
   @commands.bot_has_permissions(ban_members=True)
   @commands.cooldown(2, 1, type=commands.BucketType.user)
   async def Unban(self, ctx, member: nextcord.User = None):
+    if member == None:
+      await ctx.reply(f"{error} | Please mention a member!")
     guild = ctx.guild
     embed = nextcord.Embed(title=f'{success} | Unbanned Successfully', description=f"{member} Unbanned by {ctx.author.mention}", colour=clr)
     embed.set_footer(text=footer)
