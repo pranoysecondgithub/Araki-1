f974d1e66bd0a75f2800267bf70acee756d6c761
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-03-09 08:15:58 +0000

    Leave help command update

--- extras/helpwelc.py
+++ extras/helpwelc.py
@@ -8,14 +8,17 @@ class HelpWelcome(commands.Cog):
   def __init__(self, client):
     self.client = client
     
-  @commands.command(aliases=['help welcome', 'welcome-help'])
-  async def Welcome(self, ctx):
+  @commands.command(aliases=['help config', 'config-help'])
+  async def Config(self, ctx):
     prefix = await predb.find_one({"guild": ctx.guild.id})
     pre = prefix['prefix']
-    embed = nextcord.Embed(title='Want help to setup welcome ?', description='This way you can setup welcome easyly.', colour=clr)
+    embed = nextcord.Embed(title='Setprefix', description='It will change the server prefix.', colour=clr)
     embed.add_field(name=f"{pre}set-welcome", value="It will set the welcome channel for you")
     embed.add_field(name=f"{pre}remove-welcome", value="It will remove the welcome channel for you")
+    embed.add_field(name=f"{pre}set-leave", value="It will set the leave channel for you")
+    embed.add_field(name=f"{pre}remove-set", value="It will remove the leave channel for you")    
     embed.set_footer(text=footer, icon_url=ctx.author.avatar.url)
+    embed.set_author(name="Config help.", icon_url=ctx.author.avatar.url)
     await ctx.send(embed=embed)
       
 def setup(client):
