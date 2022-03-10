3186c1dcc7ddac85746959556e5d9ef139a26264
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-03-10 05:11:51 +0000

    Fixed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 from datetime import datetime
 from config import *
 from motor.motor_asyncio import AsyncIOMotorClient
-from time import strftime, gmtimefrom
+from time import strftime
 
 
 intents = nextcord.Intents.all()
