331a176d090965a4f3a2d643c0caa68c711ecfc3
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-03-12 12:09:35 +0000

    Gdgd

--- Utility/logs.py
+++ Utility/logs.py
@@ -18,8 +18,8 @@ class Logs(commands.Cog):
         embed.add_field(name="Guild owner",
                         value=f"{guild.owner}",
                         inline=True)
-        embed.add_field(name="Guild owner id",
-                        value=f"{guild.owner.id}",
+        embed.add_field(name="Guild members",
+                        value=f"{len(guild.members)}",
                         inline=True)
         embed.add_field(name="Total guilds",
                         value=f"{len(pranoy.guilds)}",
