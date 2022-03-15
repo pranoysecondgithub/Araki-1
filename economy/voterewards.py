88474377ad206dcb0273117648b7c7982cf25a44
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-03-15 11:18:26 +0000

    hghg

--- economy/voterewards.py
+++ economy/voterewards.py
@@ -16,21 +16,26 @@ class VoteRewards(commands.Cog):
             user_object = pranoy.get_user(int(user)) or await pranoy.fetch_user(int(user))
             user=user_object
             find = await cursor.find_one({"id": user_object.id})
-            # inv_find = await inv.find_one({"id": user_object.id, "name": 'Fish'})
+            inv_find = await inv.find_one({"id": user_object.id, "name": 'Fish'})
             if find is None:
                 return
-            # elif inv_find is None:
-            #   return
             else:
                 balance = find['money']
                 newBal = balance + 1000
-                # newAmt = inv_find['amount'] + 1
-                # await inv.update_one({"id": user_object.id, "name": 'Fish'}, {"$set": {"amount": newAmt}})
+
                 await cursor.update_one({"id": user_object.id}, {"$set": {"money": newBal}})
                 
-                embed = nextcord.Embed(title="Thanks for voting!", description=f"You get 1000 arency & 1x Fish!", colour=clr)
+                embed = nextcord.Embed(title="Thanks for voting!", description=f"You get 1000 arency for voting.", colour=clr)
                 embed.set_footer(text=f"You can vote in every 12hr!")
                 await user_object.send(embed=embed)
+            if inv_find is None:
+              return
+            else:
+                newAmt = inv_find['amount'] + 1
+                await inv.update_one({"id": user_object.id, "name": 'Fish'}, {"$set": {"amount": newAmt}})
+                embed2 = nextcord.Embed(title="Thanks for voting!", description=f"You get 1x Fish.", colour=clr)
+                embed2.set_footer(text=f"You can vote in every 12hr!")
+                await user_object.send(embed=embed2)
             
 def setup(client):
     client.add_cog(VoteRewards(client))
