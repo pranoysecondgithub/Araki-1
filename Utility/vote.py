802bb3179a2317723fa6799b205cb0db76062b49
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-04-02 20:41:05 +0000

    w

--- Utility/vote.py
+++ Utility/vote.py
@@ -16,8 +16,8 @@ class Vote(commands.Cog):
   async def Vote(self,ctx):
       top = Button(label="Top.gg", url=topgg, emoji="<:topgg:952288172446998528>")
       embed = nextcord.Embed(title="Vote me", description="You can vote in every 12hr and claim these rewards.", colour = clr)
-    embed.add_field(name="Rewards:-", value=f":fish: Fish ```1x```\n{arency} Arency 1000")
-    embed.set_footer(text="Vote me to get these rewards.")
+      embed.add_field(name="Rewards:-", value=f":fish: Fish ```1x```\n{arency} Arency 1000")
+      embed.set_footer(text="Vote me to get these rewards.")
       myVote.add_item(top)
       await ctx.send(embed=embed, view=myVote)
 def setup(client):
