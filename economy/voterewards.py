cfe8f4d213b7c6e1f94169da6e7e9616b9b51fc3
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-04-03 04:30:32 +0000

    ssaaadcccc

--- economy/voterewards.py
+++ economy/voterewards.py
@@ -3,41 +3,59 @@ from main import *
 from config import *
 import re
 
+
 class VoteRewards(commands.Cog):
     def __init__(self, client):
         self.client = client
-        
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        
+
         if message.channel.id == 952278017571442750:
             data = message.content.split(" ")
             user = re.sub("\D", "", data[4])
-             
-            user_object = pranoy.get_user(user) or await pranoy.fetch_user(user)
-            user=user_object
+
+            user_object = pranoy.get_user(user) or await pranoy.fetch_user(user
+                                                                           )
+            user = user_object
             find = await cursor.find_one({"id": user.id})
             inv_find = await inv.find_one({"id": user.id, "name": 'Fish'})
-            embed = nextcord.Embed(title="Thanks for voting!", description=f"You can vote in every 12hr!", colour=clr)
+            embed = nextcord.Embed(title="Thanks for voting!",
+                                   description=f"You can vote in every 12hr!",
+                                   colour=clr)
+
             if find is None:
                 return
             else:
                 balance = find['money']
                 newBal = balance + 1000
 
-                await cursor.update_one({"id": user.id}, {"$set": {"money": newBal}})
-                embed.add_field(name="Arency", value="1000 Arency added to your wallet for voting!")
-                
-                embed.set_footer(text=f"You can vote in every 12hr!")
-                await user_object.send(embed=embed)
+                await cursor.update_one({"id": user.id},
+                                        {"$set": {
+                                            "money": newBal
+                                        }})
+                embed.add_field(
+                    name="Arency",
+                    value="1000 Arency added to your wallet for voting!")
+
             if inv_find is None:
-              return
+                return
             else:
                 newAmt = inv_find['amount'] + 1
-                await inv.update_one({"id": user.id, "name": 'Fish'}, {"$set": {"amount": newAmt}})
-                embed.add_field(name='Inventory', value='1x Fish added to your inventory for voting!')
+                await inv.update_one({
+                    "id": user.id,
+                    "name": 'Fish'
+                }, {"$set": {
+                    "amount": newAmt
+                }})
+
+                embed.add_field(
+                    name='Inventory',
+                    value='1x Fish added to your inventory for voting!')
             embed.set_footer(text=f"You can vote in every 12hr!")
 
             await user_object.send(embed=embed)
+
+
 def setup(client):
     client.add_cog(VoteRewards(client))
