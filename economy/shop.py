dfff66c2ad2796bef6beb32980921f07d1a5bc49
Pranoy Majumdar <99827781+PranoyPlayz@users.noreply.github.com>
2022-04-04 03:44:07 +0000

    sdfsdf

--- economy/shop.py
+++ economy/shop.py
@@ -21,6 +21,21 @@ class Shop(commands.Cog):
         embed.add_field(name=f":pizza: Pizza -- 100000 | {arency}", value=f"To buy this item type ```{prefix}buy pizza```")
         embed.add_field(name=f"<:frequentcirclet:949535577806626836> Frequent Circlet -- 1000000 | {arency}", value=f"To buy this item type ```{prefix}buy frequent```")
         await ctx.send(embed=embed)
+
+    @nextcord.slash_command(name='shop', description='View the shop')
+    async def Shop(self, ctx:nextcord.Interaction):
+        pre = await predb.find_one({"guild": ctx.guild.id})
+        prefix = pre['prefix']
+        embed = nextcord.Embed( colour=clr)
+        embed.set_author(name=f"{ctx.user.name}'s Inventory")
+        embed.set_footer(text=f"To check your inventory type {prefix}inv", icon_url=ctx.user.avatar.url)
+        embed.add_field(name=f":candy: Candy -- 1000 | {arency}", value=f"To buy this item type ```{prefix}buy candy```")
+        embed.add_field(name=f":fish: Fish -- 5000 | {arency}", value=f"To buy this item type ```{prefix}buy fish```")
+        embed.add_field(name=f":cookie: Cookie -- 50000 | {arency}", value=f"To buy this item type ```{prefix}buy cookie```")
+        embed.add_field(name=f":rose: Rose -- 70000 | {arency}", value=f"To buy this item type ```{prefix}buy rose```")
+        embed.add_field(name=f":pizza: Pizza -- 100000 | {arency}", value=f"To buy this item type ```{prefix}buy pizza```")
+        embed.add_field(name=f"<:frequentcirclet:949535577806626836> Frequent Circlet -- 1000000 | {arency}", value=f"To buy this item type ```{prefix}buy frequent```")
+        await ctx.response.send_message(embed=embed)
     
     @commands.command()
     async def Buy(self, ctx, item = None):
@@ -189,7 +204,177 @@ class Shop(commands.Cog):
                     
                         else:
                             await ctx.reply("You dont have arency to buy this item!")
-                            
+
+    @nextcord.slash_command(name='buy', description='Buy a item')
+    async def Buys(
+        self,
+        ctx:nextcord.Interaction,
+        item: str = nextcord.SlashOption(name='item-id', description='Please provide a item id.')
+
+        ):
+        find = await cursor.find_one({"id": ctx.user.id})
+        pre = await predb.find_one({"guild": ctx.guild.id})
+        prefix = pre['prefix']
+        inv_find = await inv.find_one({"id": ctx.user.id})
+        if find is None:
+            await ctx.response.send_message(f"You don't have a profile!\nPlease execute the `{prefix}create or /create` command to create a profile!", ephemeral=True)
+            return
+        else:
+            bal_find = await cursor.find_one({"id": ctx.user.id})
+            balance = bal_find['money']
+            if inv_find is None:
+                insert = {"id": ctx.user.id, "name": 'Candy', "amount": 0}
+                insert2 = {"id": ctx.user.id, "name": 'Fish', "amount": 0}
+                insert3 = {"id": ctx.user.id, "name": 'Cookie', "amount": 0}
+                insert4 = {"id": ctx.user.id, "name": 'Rose', "amount": 0}
+                insert5 = {"id": ctx.user.id, "name": 'Pizza', "amount": 0}
+                insert6 = {"id": ctx.user.id, "name": 'Frequent Circlet', "amount": 0}
+                await inv.insert_one(insert)
+                await inv.insert_one(insert2)
+                await inv.insert_one(insert3)
+                await inv.insert_one(insert4)
+                await inv.insert_one(insert5)
+                await inv.insert_one(insert6)
+                if item == "candy":
+                    if balance > 999:
+                        candy_find = await inv.find_one({"id": ctx.user.id, "name": 'Candy'})
+                        newAmt = candy_find['amount'] + 1
+                        newBal = int(balance) - 1000
+                        await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                        await inv.update_one({"id": ctx.user.id, "name": 'Candy'}, {"$set": {"amount": newAmt}})
+                        await ctx.response.send_message(f"{success} | You bought 1x candy")
+                    else:
+                        await ctx.response.send_message("You dont have arency to buy this item!")
+                        
+                if item == "fish":
+                        if balance > 4999:
+                            fish_find = await inv.find_one({"id": ctx.user.id, "name": 'Fish'})
+                            newAmt = fish_find['amount'] + 1
+                            newBal = balance - 5000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Fish'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x fish")
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                if item == "cookie":
+                        if balance > 4999:
+                            cookie_find = await inv.find_one({"id": ctx.user.id, "name": 'Cookie'})
+                            newAmt = cookie_find['amount'] + 1
+                            newBal = balance - 5000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Cookie'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Cookie")
+                   
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                
+                if item == "rose":
+                        if balance > 69999:
+                            rose_find = await inv.find_one({"id": ctx.user.id, "name": 'Rose'})
+                            newAmt = rose_find['amount'] + 1
+                            newBal = balance - 70000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Rose'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Rose")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                
+                if item == "pizza":
+                        if balance > 99999:
+                            pizza_find = await inv.find_one({"id": ctx.user.id, "name": 'Pizza'})
+                            newAmt = pizza_find['amount'] + 1
+                            newBal = balance - 100000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Pizza'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Pizza")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                    
+                if item == "frequent":
+                        if balance > 999999:
+                            freq_find = await inv.find_one({"id": ctx.user.id, "name": 'Frequent Circlet'})
+                            newAmt = freq_find['amount'] + 1
+                            newBal = balance - 1000000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Frequent Circlet'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Frequent Circlet")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+            #2ns
+            else:
+                if item == "candy":
+                    if balance > 999:
+                        candy_find = await inv.find_one({"id": ctx.user.id, "name": 'Candy'})
+                        newAmt = candy_find['amount'] + 1
+                        newBal = balance - 1000
+                        await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                        await inv.update_one({"id": ctx.user.id,"name": 'Candy'}, {"$set": {"amount": newAmt}})
+                        await ctx.response.send_message(f"{success} | You bought 1x Candy")
+                    else:
+                        await ctx.response.send_message("You dont have arency to buy this item!")
+                        
+                if item == "fish":
+                        if balance > 4999:
+                            fish_find = await inv.find_one({"id": ctx.user.id, "name": 'Fish'})
+                            newAmt = fish_find['amount'] + 1
+                            newBal = balance - 5000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Fish'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Fish")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                
+                if item == "cookie":
+                        if balance > 4999:
+                            cookie_find = await inv.find_one({"id": ctx.user.id, "name": 'Cookie'})
+                            newAmt = cookie_find['amount'] + 1
+                            newBal = balance - 5000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Cookie'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Cookie")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                
+                if item == "rose":
+                        if balance > 69999:
+                            rose_find = await inv.find_one({"id": ctx.user.id, "name": 'Rose'})
+                            newAmt = rose_find['amount'] + 1
+                            newBal = balance - 70000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Rose'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Rose")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                    
+                if item == "pizza":
+                        if balance > 99999:
+                            pizza_find = await inv.find_one({"id": ctx.user.id, "name": 'Pizza'})
+                            newAmt = pizza_find['amount'] + 1
+                            newBal = balance - 100000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Pizza'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Pizza")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
+                    
+                if item == "frequent":
+                        if balance > 999999:
+                            freq_find = await inv.find_one({"id": ctx.user.id, "name": 'Frequent Circlet'})
+                            newAmt = freq_find['amount'] + 1
+                            newBal = balance - 1000000
+                            await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal}})
+                            await inv.update_one({"id": ctx.user.id, "name": 'Frequent Circlet'}, {"$set": {"amount": newAmt}})
+                            await ctx.response.send_message(f"{success} | You bought 1x Frequent Circlet")
+                    
+                        else:
+                            await ctx.response.send_message("You dont have arency to buy this item!")
                 
 def setup(client):
     client.add_cog(Shop(client))
--- economy/slots.py
+++ economy/slots.py
@@ -29,22 +29,21 @@ class Slots(commands.Cog):
         slot2 = slots[random.randint(0, 5)]
         slot3 = slots[random.randint(0, 5)]
         slotOutput = '| :{}: | :{}: | :{}: |\n'.format(slot1, slot2, slot3)
-        embed1 = nextcord.Embed(title = "Slots Machine", color = clr)
+        embed1 = nextcord.Embed(title = "Slots Machine", colour = clr)
         embed1.add_field(name = "{}\nWon".format(slotOutput), value = f'You won {amount + amount:,} arency')
         embed1.set_footer(text=footer, icon_url=ctx.author.avatar.url)
         newBal1 = check['money'] + amount + amount
 
-        won = nextcord.Embed(title = "Slots Machine", color = clr)
+        won = nextcord.Embed(title = "Slots Machine", colour = clr)
         won.add_field(name = "{}\nWon".format(slotOutput), value = f'You won {amount:,} arency')
         won.set_footer(text=footer, icon_url=ctx.author.avatar.url)
         newBal2 = check['money'] + amount
 
-        lost = nextcord.Embed(title = "Slots Machine", color = clr)
+        lost = nextcord.Embed(title = "Slots Machine", colour = clr)
         lost.add_field(name = "{}\nLost".format(slotOutput), value = f'You lost {amount:,} arency')
         lost.set_footer(text=footer, icon_url=ctx.author.avatar.url)
         newBal3 = check['money'] - amount
         if slot1 == slot2 == slot3:
-           await ctx.send(embedw=won)
            await cursor.update_one({"id": ctx.author.id}, {"$set": {"money": newBal1}})
            await ctx.send(embed=won)
 
@@ -54,6 +53,58 @@ class Slots(commands.Cog):
         else:
           await ctx.send(embed = lost)
           await cursor.update_one({"id": ctx.author.id}, {"$set": {"money": newBal3}})
+          return
+
+
+  @nextcord.slash_command(name='slots', description='Use slots machine')
+  async def SlotsS(
+        self,
+        ctx:nextcord.Interaction,
+        amount: int = nextcord.SlashOption(name='amount', description='Please provide a amount')
+    ):
+    prefixcheck = await predb.find_one({"guild": ctx.guild.id})
+    prefix = prefixcheck['prefix']
+    check = await cursor.find_one({"id": ctx.user.id})
+    if check is None:
+        await ctx.message.reply(f"You don't have a profile!\nPlease execute the `{prefix}create or /create` command to create a profile!")
+        return
+    else:
+      balance = check['money']
+      if balance <= 0:
+        await ctx.response.send_message("Your balance is too low!")
+      elif balance < int(amount):
+        await ctx.response.send_message("Your balance is too low!")
+      else:
+      
+        slots = ['bus', 'train', 'horse', 'tiger', 'monkey', 'cow']
+        slot1 = slots[random.randint(0, 5)]
+        slot2 = slots[random.randint(0, 5)]
+        slot3 = slots[random.randint(0, 5)]
+        slotOutput = '| :{}: | :{}: | :{}: |\n'.format(slot1, slot2, slot3)
+        embed1 = nextcord.Embed(title = "Slots Machine", colour = clr)
+        embed1.add_field(name = "{}\nWon".format(slotOutput), value = f'You won {amount + amount:,} arency')
+        embed1.set_footer(text=footer, icon_url=ctx.user.avatar.url)
+        newBal1 = check['money'] + amount + amount
+
+        won = nextcord.Embed(title = "Slots Machine", colour = clr)
+        won.add_field(name = "{}\nWon".format(slotOutput), value = f'You won {amount:,} arency')
+        won.set_footer(text=footer, icon_url=ctx.user.avatar.url)
+        newBal2 = check['money'] + amount
+
+        lost = nextcord.Embed(title = "Slots Machine", colour = clr)
+        lost.add_field(name = "{}\nLost".format(slotOutput), value = f'You lost {amount:,} arency')
+        lost.set_footer(text=footer, icon_url=ctx.user.avatar.url)
+        newBal3 = check['money'] - amount
+        if slot1 == slot2 == slot3:
+           await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal1}})
+           await ctx.response.send_message(embed=won)
+
+        if slot1 == slot2:
+          await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal2}})
+          await ctx.response.send_message(embed=embed1)
+        else:
+          await ctx.response.send_message(embed = lost)
+          await cursor.update_one({"id": ctx.user.id}, {"$set": {"money": newBal3}})
           return
 def setup(client):
   client.add_cog(Slots(client))
