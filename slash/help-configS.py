41d8546299d8ab79af4f18dc56dc243771877a3f
PranoyPlayz <kumarricky252@gmail.com>
2022-04-13 05:04:48 +0000

    fwefw

--- slash/help-configS.py
+++ slash/help-configS.py
@@ -12,15 +12,15 @@ class HelpConfigS(commands.Cog):
   async def ConfigS(
     self,
     interaction:nextcord.Interaction,
-    type: str = nextcord.SlashOption(name="choose-type", description='Pleasechoose a config type.', choices=['Welcome', 'Configs'])
+    type: int = nextcord.SlashOption(name="choose-type", description='Pleasechoose a config type.', choices={'Welcome': 1, 'Configs': 2})
     ):
-    if type == 'Welcome':
+    if type == 1:
       embed1 = nextcord.Embed(title='Welcome Setup', description='Well these are the few steps that you can setup welcome easyly.', colour=clr)
       embed1.add_field(name='Variables', value="``[user]``` Mentions the new member.\n```[userName]``` New member name without mentioning\n```[memberCount]``` Amount of members reached\n```[server]``` Server name\n```[joinTime]``` User join time\n```[userAge]``` View the age of the user account")
       embed1.add_field(name='Welcome setup commands', value='```\nenable-welcome, Set-welcome, welcome-author, welcome-author-icon, welcome-title, welcome-description, welcome-thumbnail, welcome-image, welcome-footer, welcome-footer-icon\n```')
       embed1.set_footer(text=footer)
       await interaction.response.send_message(embed=embed1)
-    if type == 'Configs':
+    if type == 2:
       embed2 = nextcord.Embed(title='/Setprefix', description='It will change the server prefix.', colour=clr)
       embed2.add_field(name=f"/set-welcome", value="It will set the welcome channel for you")
       embed2.add_field(name=f"/remove-welcome", value="It will remove the welcome channel for you")
