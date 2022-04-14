cf9c967a9fc1187defd4d4b416fd0ce566e534e7
Pranoy Majumdar <kumarricky252@gmail.com>
2022-04-14 18:45:39 +0000

    Update config

--- slash/help-configS.py
+++ slash/help-configS.py
@@ -11,16 +11,7 @@ class HelpConfigS(commands.Cog):
   @nextcord.slash_command(name='help-config', description='This shows the help page of config')
   async def ConfigS(
     self,
-    interaction:nextcord.Interaction,
-    type: int = nextcord.SlashOption(name="choose-type", description='Pleasechoose a config type.', choices={'Welcome': 1, 'Configs': 2})
-    ):
-    if type == 1:
-      embed1 = nextcord.Embed(title='Welcome Setup', description='Well these are the few steps that you can setup welcome easyly.', colour=clr)
-      embed1.add_field(name='Variables', value="``[user]``` Mentions the new member.\n```[userName]``` New member name without mentioning\n```[memberCount]``` Amount of members reached\n```[server]``` Server name\n")
-      embed1.add_field(name='Welcome setup commands', value='```\nenable-welcome, Set-welcome, welcome-author, welcome-author-icon, welcome-title, welcome-description, welcome-thumbnail, welcome-image, welcome-footer, welcome-footer-icon\n```')
-      embed1.set_footer(text=footer)
-      await interaction.response.send_message(embed=embed1)
-    if type == 2:
+    interaction:nextcord.Interaction):
       embed2 = nextcord.Embed(title='/Setprefix', description='It will change the server prefix.', colour=clr)
       embed2.add_field(name=f"/set-welcome", value="It will set the welcome channel for you")
       embed2.add_field(name=f"/remove-welcome", value="It will remove the welcome channel for you")
