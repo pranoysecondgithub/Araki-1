19bae16f1fc7a263ba77bd5d1e7e8a4620387bf0
Pranoy Majumdar <kumarricky252@gmail.com>
2022-04-15 06:26:57 +0000

    hfbrgbh

--- slash/setup-welcome.py
+++ slash/setup-welcome.py
@@ -284,6 +284,132 @@ class wlcm_setup(commands.Cog):
     async def reset_welcome(self, ctx:nextcord.Interaction):
       await ctx.response.send_message("Which components you want to reset ?", view=WelcomeReset())
 
+    @commands.command(name='test-welcome')
+    async def _test_welcome(self, ctx):
+      find = await welcome.find_one({"guild": ctx.guild.id})
+      find_msg = await wlcm_msg.find_one({"guild": ctx.guild.id})
+      author = find_msg['author']
+      author_icon = find_msg['author_icon']
+      title = find_msg['title']
+      desc = find_msg['description']
+      image = find_msg['image']
+      thumbnail = find_msg['thumbnail']
+      footer = find_msg['footer']
+      footer_icon = find_msg['footer_icon']
+      msg = find_msg['message']
+      if find and find_msg is None:
+        return
+      elif author == None and author_icon == None and title == None and desc == None and image == None and thumbnail == None and footer == None and footer_icon == None and msg == None:
+        return
+      else:
+        author = find_msg['author']
+        author_icon = find_msg['author_icon']
+        title = find_msg['title']
+        desc = find_msg['description']
+        image = find_msg['image']
+        thumbnail = find_msg['thumbnail']
+        footer = find_msg['footer']
+        footer_icon = find_msg['footer_icon']
+        msg = find_msg['message']
+        channel = find['channel']
+
+        embed = nextcord.Embed(colour=clr)
+        if author != '' and author != None:
+          if '[userName]' in author:
+            author = author.replace('[userName]', ctx.author.name)
+        if author != '' and author != None:
+          if '[server]' in author:
+            author = author.replace('[server]', ctx.guild.name)
+        if author != '' and author != None:
+          if '[memberCount]' in author:
+            author = author.replace('[memberCount]', len(ctx.guild.members))
+
+        #title
+        if title != '' and title != None:
+          if '[userName]' in title:
+            title = title.replace('[userName]', ctx.author.name)
+        if title != '' and title != None:
+          if '[user]' in title:
+            title = title.replace('[user]', ctx.author.mention)
+        if title != '' and title != None:
+          if '[memberCount]' in title:
+            title = title.replace('[memberCount]', len(ctx.guild.members))
+        if title != '' and title != None:
+          if '[server]' in title:
+            title = title.replace('[server]', ctx.guild.name)
+        #desc 
+        if desc != '' and desc != None:
+          if '[userName]' in desc:
+            desc = desc.replace('[userName]', ctx.author.name)
+        if desc != '' and desc != None:
+          if '[user]' in desc:
+            desc = desc.replace('[user]', ctx.author.mention)
+        if desc != '' and title != None:
+          if '[memberCount]' in desc:
+            desc = desc.replace('[memberCount]', len(ctx.guild.members))
+        if desc != '' and desc != None:
+          if '[server]' in desc:
+            desc = desc.replace('[server]', ctx.guild.name)
+
+        #message
+        if msg != '' and msg != None:
+          if '[user]' in msg:
+            msg = msg.replace('[user]', ctx.author.mention)
+        if msg != '' and msg != None:
+          if '[userName]' in msg:
+            msg = msg.replace('[userName]', ctx.author.name)
+        if msg != '' and msg != None:
+          if '[server]' in msg:
+            msg = msg.replace('[server]', ctx.guild.name)
+
+        # images
+        if author_icon != '' and author_icon != None:
+          if '[avatar]' in avatar_icon:
+            avatar_icon = avatar_icon.replace('[avatar]', ctx.author.avatar.url)
+        if thumbnail != '' and thumbnail != None:
+          if '[avatar]' in thumbnail:
+            thumbnail = thumbnail.replace('[avatar]', ctx.author.avatar.url)
+        if footer_icon != '' and footer_icon != None:
+          if '[avatar]' in footer_icon:
+            footer_icon = footer_icon.replace('[avatar]', ctx.author.avatar.url)
+        if image != '' and image != None:
+          if '[avatar]' in image:
+            image = image.replace('[avatar]', ctx.author.avatar.url)
+
+        #footer
+        if footer != '' and footer != None:
+          if '[user]' in footer:
+            footer = footer.replace('[user]', ctx.author.mention)
+        if footer != '' and footer != None:
+          if '[userName]' in footer:
+            footer = footer.replace('[userName]', ctx.author.name)
+        if footer != '' and footer != None:
+          if '[server]' in footer:
+            footer = footer.replace('[server]', ctx.guild.name)
+
+
+
+        if author != None:
+          embed.set_author(name=author)
+        if author_icon != None:
+          embed.set_author(icon_url=f"{author_icon}")
+        if title != None:
+          embed.title=title
+        if desc != None:
+          embed.description=desc
+        if image != None:
+          embed.set_image(url=f"{image}")
+        if thumbnail != None:
+          embed.set_thumbnail(url=f"{thumbnail}")
+        if footer != None:
+          embed.set_footer(text=footer)
+        if footer_icon != None:
+          embed.set_footer(icon_url=f"{footer_icon}")
+        if msg != None :
+          await pranoy.get_channel(channel).send(msg, embed=embed)
+        else:
+          await pranoy.get_channel(channel).send(embed=embed)
+
 
 def setup(client):
     client.add_cog(wlcm_setup(client))
